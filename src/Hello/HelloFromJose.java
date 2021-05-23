3895e13adce635238c597179a6fbe8bea6a8ba05
Samir Rodriguez <samirrodriguez14@gmail.com>
2021-05-22 23:42:17 -0400

    Printed a hello message

--- src/Hello/HelloFromJose.java
+++ src/Hello/HelloFromJose.java
@@ -2,6 +2,8 @@ package Hello;
 
 public class HelloFromJose {
     public static void main(String[] args) {
+
         System.out.println("Hello");
+        System.out.println("Hello Jose, my name is Samir");
     }
 }
